e272b724807d35e2a72263a1e6780f5b4080f00f
Daniel Díaz Astudillo <daniel.diaz.astudillo@gmail.com>
2018-09-21 00:47:28 -0300

    ScrollTo service implemented in accordion

--- src/app/web-views/assist-services/assist-services.component.ts
+++ src/app/web-views/assist-services/assist-services.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AssistanceService } from '../../services/assistance.service';
 import { IAssistServices } from '../../interfaces/assist-services.interface';
 import { AssistanceListenerService } from '../../services/assistance-listener.service';
+import { ScrollToService, ScrollToConfigOptions } from '@nicky-lenaers/ngx-scroll-to';
 
 @Component({
   selector: 'app-assist-services',
@@ -12,13 +13,19 @@ export class AssistServicesComponent implements OnInit {
   @Output() selectedService = new EventEmitter<IAssistServices>();
 
   constructor(private _assistSvc: AssistanceService,
-              private _assistListenerSvc: AssistanceListenerService) { }
+              private _assistListenerSvc: AssistanceListenerService,
+              private _scrollToService: ScrollToService) { }
 
   currentServices: IAssistServices[];
+
+  scrollToFormConfig: ScrollToConfigOptions = {
+    target: 'inicio'
+  };
 
   serviceFromAccordion(service: IAssistServices) {
     this.selectedService.emit(service);
     this._assistListenerSvc.setAssistance(service);
+    this._scrollToService.scrollTo(this.scrollToFormConfig);
   }
 
   ngOnInit() {
